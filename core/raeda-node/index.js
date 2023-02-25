cfeca83291763bc430b63020fab74c4521f1e5e6
Toby <toby@tobias.codes>
2023-02-25 16:19:14 -0700

    start on raeda node package

---
+++ core/raeda-node/index.js
@@ -0,0 +1,16 @@
+// function getBids(){
+// 	return [{'':'from','from':'from','from':'from'}];
+// }
+
+function lakePost({id,lakeId,lakeAddress,lakeName,maxPrice,iX,fX,exp,iT=null,fT=null,}){
+	// initPost(id,lakeId,lakeAddress,lakeName,maxPrice,iX,fX,exp)
+	// if (iT!=null) setInitialTime(iT)
+	// if (fT!=null) setFinalTime(fT)
+	
+	// price
+	// blabla
+}
+
+function lakeBid({}){
+	
+}
