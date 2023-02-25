7e88c326afbfeac8e80c3eef305064657b8191e1
Toby <toby@tobias.codes>
2023-02-24 18:12:12 -0700

    from browser to rust and back

--- dapp/index.js
+++ dapp/index.js
@@ -1,3 +1,5 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 const express = require('express')
 const app = express()
 const http = require('http');
@@ -13,8 +15,28 @@ app.set('view engine', 'pug');
 
 const port = process.env.PORT || 3000;
 
+var fetchid = 0;
+function raedaAPIReqObj(method,data=null){
+	fetchid += 1;
+	var resobj = {"jsonrpc": "2.0", "method": method, "id":fetchid };
+	if (data!==null) resobj['data'] = data;
+	return resobj;
+}
+function raedaAPICall(method,data=null){
+	return fetch('http://localhost:3030/say_hello', {
+		method: 'post',
+		body: JSON.stringify(raedaAPIReqObj(method,data)),
+		headers: {'Content-Type': 'application/json'}
+	}).then((res) => res.json()).then((body) => {
+		return body;
+	});
+}
+
 app.get('/', (req, res) => {
-	res.render('index');
+	raedaAPICall('say_hello').then((rust_res)=>{
+		console.log(rust_res)
+		res.render('index',{rust_res:rust_res['result']});
+	});
 })
 app.get('/favicon', (req, res) => {
 	res.send('no favicon yet');
