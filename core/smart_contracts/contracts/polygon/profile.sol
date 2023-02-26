856188173008041774fa6fb03cc4773035648e20
Rishin Madan <rishin.madan@gmail.com>
2023-02-26 16:33:16 +0000

    smart contract development

--- core/smart_contracts/contracts/polygon/profile.sol
+++ core/smart_contracts/contracts/polygon/profile.sol
@@ -1,8 +1,36 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract profile {
+
+    uint private userIdCount = -1;
+
+    enum waterTypes {LAKE,RIVER}
+
     //Profile contains: id,name,EOAs,brief summary, lake/river
     struct Profile {
-        uint 
+        // uint _userId;
+        waterTypes waterType;
+        string userName;
+        mapping(address => bool) EOAs;
+        string summary;         
     }
+
+    mapping(uint => Profile) public profiles;
+    event profileEvent(uint indexed _userId, waterTypes indexed _waterType, string _userName, string _summary);
+
+    function addProfile(waterTypes _waterType,string calldata _userName, string calldata _summary) external {
+        userIdCount +=1;
+        profiles[userIdCount].waterType = _waterType;
+        profiles[userIdCount].EOAs[msg.sender]=true;
+        profiles[userIdCount].summary = _summary;
+        emit profileEvent(userIdCount,_waterType,_userName,_summary)
+    }
+
+    function addEOA(uint _userId, address newEOA) external {
+        require(profiles[_userId].EOAs[msg.sender]);
+        profiles[_userId].EOAs[msg.sender]=true;
+
+    }
+
+
 }
