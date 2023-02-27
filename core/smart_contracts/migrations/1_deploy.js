c36b01b3c652f4ebb911f8799e848ca96d383f73
Rishin Madan <rishin.madan@gmail.com>
2023-02-27 21:08:00 +0000

    Smart contract edits, set-up subgraph

--- core/smart_contracts/migrations/1_deploy.js
+++ core/smart_contracts/migrations/1_deploy.js
@@ -3,8 +3,8 @@ const waterSource = artifacts.require("waterSource");
 const lake = artifacts.require("lake");
 const river = artifacts.require("river");
 
-module.exports = function (deployer) {
-  deployer.deploy(profile).then(function(){
+module.exports = async function (deployer) {
+  await deployer.deploy(profile).then(function(){
     // console.log(profile.address);
     return deployer.deploy(lake,profile.address);
     
