053e5bcdfaf26e7be3c64ba9af78636011a81cca
rishin01 <111346158+rishin01@users.noreply.github.com>
2023-03-05 06:43:09 +0000

    Delete core/polygonID/Issuer/issuer-deployed directory

--- core/polygonID/Issuer/issuer-deployed/sh-id-platform/internal/loader/http.go
+++
@@ -1,8 +0,0 @@
-package loader
-
-import "github.com/iden3/go-schema-processor/loaders"
-
-// HTTPFactory returns an http loader
-func HTTPFactory(u string) Loader {
-	return &loaders.HTTP{URL: u}
-}
